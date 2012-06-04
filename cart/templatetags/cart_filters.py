2cbad91dc9f18a4344980c6b05912945786f0872
Nimrod S. Kerrett <unclezzzen@gmail.com>
2012-06-04 19:20:30 +0700

    added cart_quantity template filter

--- cart/templatetags/cart_filters.py
+++ cart/templatetags/cart_filters.py
@@ -1,14 +1,19 @@
 """
-    Example:
+    Examples:
         {% include cart_filters %}
-        {{ product|cart_product_as_p:CART }}
-    In order to have CART,
-    you need to add 'cart.context_processors.cart'
+        {{ product|cart_quantity:CART }} {# quantity in cart (or 0) #}
+        {{ product|cart_product_as_p:CART }} {# render product's cart form as <p> #}
+        {{ product|cart_product_as_ul:CART }} {# render form as <ul> #}
+    In order to have CART, you need to add 'cart.context_processors.cart'
     to TEMPLATE_CONTEXT_PROCESSORS
 """
 from django import template
 
 register = template.Library()
+
+@register.filter
+def cart_quantity(product,cart):
+    return cart.get_product_quantity(product)
 
 @register.filter(is_safe=True)
 def cart_product_as_p(product,cart):
